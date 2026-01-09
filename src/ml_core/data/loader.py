e05a9e8d1aa3e1e34647080c89f8373f47dfaa57
Stellan Tulfer <scur2385@tcn352.local.snellius.surf.nl>
2026-01-09 12:50:06 +0100

    loader

--- src/ml_core/data/loader.py
+++ src/ml_core/data/loader.py
@@ -20,10 +20,14 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     train_transform = transforms.Compose([])
     val_transform = transforms.Compose([])
     # TODO: Define Paths for X and Y (train and val)
-    x_train_path = base_path / data_cfg["x_train"]
-    y_train_path = base_path / data_cfg["y_train"]
-    x_val_path = base_path / data_cfg["x_val"]
-    y_val_path = base_path / data_cfg["y_val"]
+    x_train_name = data_cfg.get("x_train", "camelyonpatch_level_2_split_train_x.h5")
+    y_train_name = data_cfg.get("y_train", "camelyonpatch_level_2_split_train_y.h5")
+    x_val_name   = data_cfg.get("x_val",   "camelyonpatch_level_2_split_valid_x.h5")
+    y_val_name   = data_cfg.get("y_val",   "camelyonpatch_level_2_split_valid_y.h5")    
+    x_train_path = base_path / x_train_name
+    y_train_path = base_path / y_train_name
+    x_val_path   = base_path / x_val_name
+    y_val_path   = base_path / y_val_name
     # TODO: Instantiate PCAMDataset for train and val
     train_ds = PCAMDataset(
         x_path=str(x_train_path),
@@ -34,6 +38,19 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
         x_path=str(x_val_path),
         y_path=str(y_val_path),
         transform=val_transform,
+    )
+
+    labels = []
+    for _, y in train_ds:
+        labels.append(y.item())
+    labels = torch.tensor(labels, dtype=torch.long)
+    class_counts = torch.bincount(labels)
+    class_weights = 1.0 / class_counts.float()
+    sample_weights = class_weights[labels]
+    sampler = WeightedRandomSampler(
+        weights=sample_weights,
+        num_samples=len(sample_weights),
+        replacements=True,
     )
     # TODO: Create DataLoaders
     # train_loader = ...
