ac8826f54db74ea6f971de6571b2597b256e2b53
Stellan Tulfer <scur2385@tcn352.local.snellius.surf.nl>
2026-01-09 12:09:10 +0100

    loader.py implemented

--- src/ml_core/data/loader.py
+++ src/ml_core/data/loader.py
@@ -15,16 +15,41 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     data_cfg = config["data"]
     base_path = Path(data_cfg["data_path"])
 
-    # TODO: Define Transforms
     # train_transform = ...
     # val_transform = ...
-
+    train_transform = transforms.Compose([])
+    val_transform = transforms.Compose([])
     # TODO: Define Paths for X and Y (train and val)
-    
+    x_train_path = base_path / data_cfg["x_train"]
+    y_train_path = base_path / data_cfg["y_train"]
+    x_val_path = base_path / data_cfg["x_val"]
+    y_val_path = base_path / data_cfg["y_val"]
     # TODO: Instantiate PCAMDataset for train and val
-
+    train_ds = PCAMDataset(
+        x_path=str(x_train_path),
+        y_path=str(y_train_path),
+        transform=train_transform,
+    )
+    val_ds = PCAMDataset(
+        x_path=str(x_val_path),
+        y_path=str(y_val_path),
+        transform=val_transform,
+    )
     # TODO: Create DataLoaders
     # train_loader = ...
     # val_loader = ...
-    
-    raise NotImplementedError("Implement get_dataloaders")
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    return train_loader, val_loader
